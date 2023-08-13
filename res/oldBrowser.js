c947d79433c481bfcccc6d58ffacb018b20781e0
WavJaby <WavJaby@gmail.com>
2023-08-14 00:37:49 +0800

    Make old browser support

--- res/oldBrowser.js
+++ res/oldBrowser.js
@@ -1,43 +1,43 @@
 console.log('Using old browser');
 
+var requireToLoad = 4;
+
+loadScript('./res/require.js', onScriptLoad);
+loadScript('https://unpkg.com/@babel/polyfill@7.12.1/dist/polyfill.min.js', onScriptLoad);
+loadScript('https://unpkg.com/babel-standalone@6.26.0/babel.min.js', function () {
+	loadScript('https://unpkg.com/babel-preset-env-standalone@1.6.2/babel-preset-env.min.js', onScriptLoad);
+});
+loadScript('https://inexorabletash.github.io/polyfill/url.js', onScriptLoad);
+
 if (window.ieVersion) {
-	var requireToLoad = 4;
-
-	loadScript('./res/require.js', onScriptLoad);
-	loadScript('https://unpkg.com/@babel/polyfill@7.12.1/dist/polyfill.min.js', onScriptLoad);
-	loadScript('https://unpkg.com/babel-standalone@6.26.0/babel.min.js', function () {
-		loadScript('https://unpkg.com/babel-preset-env-standalone@1.6.2/babel-preset-env.min.js', onScriptLoad);
-	});
-	loadScript('https://inexorabletash.github.io/polyfill/url.js', onScriptLoad);
-
 	if (ieVersion < 10) {
 		requireToLoad += 2;
 		loadScript('https://inexorabletash.github.io/polyfill/html.js', onScriptLoad);
 		loadScript('https://inexorabletash.github.io/polyfill/dom.js', onScriptLoad);
 	}
+}
 
-	function onScriptLoad() {
-		if (--requireToLoad > 0)
-			return;
+function onScriptLoad() {
+	if (--requireToLoad > 0)
+		return;
 
-		try {
-			eval(parseScript(fetchSync('./index.js').body, './index.js', 1));
-		} catch (e) {
-			console.error(e);
-		}
-
-		if (ieVersion === 9)
-			window.onload();
+	try {
+		eval(parseScript(fetchSync('./index.js').body, './index.js', 1));
+	} catch (e) {
+		console.error(e);
 	}
 
-	function loadScript(src, onload) {
-		var script = document.createElement('script');
-		script.src = src;
-		script.onload = onload;
+	if (ieVersion === 9)
+		window.onload();
+}
 
-		if (ieVersion && ieVersion < 9)
-			document.appendChild(script);
-		else
-			document.head.appendChild(script);
-	}
+function loadScript(src, onload) {
+	var script = document.createElement('script');
+	script.src = src;
+	script.onload = onload;
+
+	if (ieVersion && ieVersion < 9)
+		document.appendChild(script);
+	else
+		document.head.appendChild(script);
 }
